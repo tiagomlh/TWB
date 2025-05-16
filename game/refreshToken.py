2f74d31ac24fb5b853ab64c4878bfe93dda17a12
tiago malmham <malmhamcdc@unochapeco.edu.br>
2025-05-15 22:39:50 -0300

    init branch refresh-session token

---
+++ game/refreshToken.py
@@ -0,0 +1,36 @@
+
+
+# https://www.tribalwars.com.br/
+
+# https://www.tribalwars.com.br/page/play/br134
+# -> location
+#    	https://br134.tribalwars.com.br/login.php?token=530282997b9019ee90a2ab9ee05e3ce366012c78bbf86625c8706abf7a3f2c67335a26f15b4b2a2693660a317f335b524fa3fcbee45c7c85fb082dfb4f3c56e4
+request header
+alt-svc
+	h3=":443";ma=86400,h3-29=":443";ma=86400,h3-27=":443";ma=86400
+content-encoding
+	gzip
+content-type
+	text/html; charset=utf-8
+date
+	Thu, 15 May 2025 02:31:09 GMT
+location
+	/game.php?screen=overview&intro
+server
+	nginx
+set-cookie
+	sid=0%3A4e85b15a4c59ea430898c39cf34e3b86ecd8058cd23d75e5d937e497e5673b5693959ae1a31a8fbf98c0df2ab5ef918bc8c61249d00cdf8da47f35d20cbacc09; path=/; secure; HttpOnly
+vary
+	Accept-Encoding
+X-Firefox-Spdy
+	h2
+x-frame-options
+	NONE
+x-said
+	1401637
+
+
+# GET
+# 	https://br134.tribalwars.com.br/game.php?screen=overview&intro=
+# Cookie
+# 	cid=2102331010; br_auth=9417b1109d60:e51d22612ed809ef505c5ee464c9f5392fc92c1d8da5ab58e19e4f8b03799edf; sid=0%3A4e85b15a4c59ea430898c39cf34e3b86ecd8058cd23d75e5d937e497e5673b5693959ae1a31a8fbf98c0df2ab5ef918bc8c61249d00cdf8da47f35d20cbacc09
